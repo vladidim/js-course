b40fe985ea168fe66212da16fe6c98935095289c
Vladislav Dimitrov <vladi@Vladislavs-MacBook-Air.local>
2022-12-22 16:45:24 +0200

    convTemp commit

---
+++ convTemp.js
@@ -0,0 +1,8 @@
+var convertTemperature = function(celsius) {
+    let output = [];
+    let kel = celsius + 273.15;
+    let fahr = (celsius * 1.80 + 32.00);
+    output.push(kel);
+    output.push(fahr);
+    return output;
+};
